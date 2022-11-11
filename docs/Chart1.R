9f345a0f5a836216b42f8651a733f7d00bbc2f81
militha-madur <militham@uw.edu>
2022-11-11 14:22:36 -0800

    added data

--- docs/Chart1.R
+++ docs/Chart1.R
@@ -1,1 +1,11 @@
-# Chart 1 - Mapping the loations/origin points of Forest Fires in the U.S.
+# Chart 1 - Mapping the locations/origin points of Forest Fires in the U.S.
+
+# loading U.S. Fire Origin Points Data
+library(dplyr)
+library(devtools)
+# devtools::install_github("redivis/redivis-r", ref="main")
+user <- redivis::user("demo")
+dataset <- user$dataset("us_forest_service_fires:5k9t:v1_1")
+fire_origin_points <- dataset$table("us_fire_origin_points:5r3s")
+View(fire_origin_points)
+ 
