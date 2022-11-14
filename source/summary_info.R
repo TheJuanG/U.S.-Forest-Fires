2cece312c7048489e2a824a32bbb06e8864234a7
Chantalle Matro <chantalle@Chantalles-MacBook-Air-2.local>
2022-11-13 23:59:17 -0800

    perimeter data set won't load

--- source/summary_info.R
+++ source/summary_info.R
@@ -1,7 +1,29 @@
-# summary_info.R 
-# A source file that takes in a dataset and returns a list of info about it:
-# summary_info <- list()
-# summary_info$num_observations <- nrow(my_dataframe)
-# summary_info$some_max_value <- my_dataframe %>%
-#   filter(some_var == max(some_var, na.rm = T)) %>%
-#   select(some_label)
+# Summary Information
+
+library(tidyverse)
+library(ggplot2)
+
+# U.S. Fire Origin Points Data
+origin_points <- read.csv("https://media.githubusercontent.com/media/info201a-au2022/project-group-2-section-af/main/data/US_Fire_Origin_Points.csv")
+View(origin_points)
+
+summary_info <- list()
+
+# info 1
+summary_info$origin_num_observations <- nrow(origin_points)
+
+# info 2
+summary_info$max_state_origin <- origin_points %>%
+  filter(STATE_NAME == max(STATE_NAME, na.rm = TRUE)) %>%
+  select(STATE_NAME)
+
+# U.S. Fire Perimeters
+perimeters <- read.csv("https://media.githubusercontent.com/media/info201a-au2022/project-group-2-section-af/main/data/US_Fire_Perimeters.csv")
+View(perimeters)
+
+# info 3
+summary_info$perimeters_num_observations <- nrow(perimeters)
+
+# info 4
+
+# info 5
