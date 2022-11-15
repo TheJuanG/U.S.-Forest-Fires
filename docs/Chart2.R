57d3e4b43f3854c602ce82134029bce9242fa200
Chantalle Matro <chantalle@Chantalles-MacBook-Air-2.local>
2022-11-14 23:01:10 -0800

    stored in var

--- docs/Chart2.R
+++ docs/Chart2.R
@@ -8,7 +8,7 @@ library(scales)
 data <- read_csv('https://media.githubusercontent.com/media/info201a-au2022/project-group-2-section-af/main/data/US_Fire_Origin_Points.csv')
 View(data)
 
-ggplot(data, aes(x=STATISTICAL_CAUSE)) + 
+graph <- ggplot(data, aes(x=STATISTICAL_CAUSE)) + 
   geom_bar(width = 0.7) +
   labs(x = "Causes", y = "Number of Fires Caused", title = "The Causes of Forest Fires") +
   scale_y_continuous(labels = label_number(suffix = " K", scale = 1e-3)) +
