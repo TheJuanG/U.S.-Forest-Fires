f818dd931e3da73db32d8c1af32658cffa78fe1f
Chantalle Matro <chantalle@Chantalles-MacBook-Air-2.local>
2022-11-14 18:34:02 -0800

    summary tables

--- source/table_info.R
+++ source/table_info.R
@@ -5,14 +5,22 @@ library(tidyverse)
 library(dplyr)
 source("~/Documents/info201/assignments/project-group-2-section-af/source/summary_info.R")
 
-# Table containing summary info
-# table_info <- data.frame(most_common_fire_size = summary_info$fire_size,
-#                       most_common_state_origin = summary_info$max_state_origin,
-#                       most_common_cause = summary_info$stat_cause,
-#                       year_with_most_fires = summary_info$year_most_fires,
-#                       largest_total_acres = prettyNum(summary_info$max_total_acres, 
-#                                             big.mark = ",", scientific = FALSE))
+# U.S. Fire Origin Points Summary Table
 
-table_info_2 <- origin_points %>% 
-  group_by(STATISTICAL_CAUSE) %>% 
-  select(INITIAL_RESPONSE, STATE_NAME, COUNTY_NAME, FIRE_NAME, FIRE_SIZE_CLASS)
+# code retrieved from 
+# https://exploratory.io/note/kanaugust/1701090969905358#:~:text=Mode%20Function,write%20a%20custom%20R%20function.
+calculate_mode <- function(x) {
+  uniqx <- unique(na.omit(x))
+  uniqx[which.max(tabulate(match(x, uniqx)))]
+}
+
+table_info <- origin_points %>%
+  group_by(STATE_NAME) %>%
+  summarize(county_most_fires = calculate_mode(COUNTY_NAME),
+            common_fire_size = calculate_mode(FIRE_SIZE_CLASS),
+            common_causes = calculate_mode(STATISTICAL_CAUSE))
+
+table_info_2 <- perimeters %>% 
+  group_by(FIREYEAR) %>% 
+  summarize(max_total_acres = max(TOTALACRES, na.rm = TRUE),
+            common_causes = calculate_mode(STATCAUSE))
